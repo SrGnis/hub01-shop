561c3407fe7103815e3023918b20ea703de8ece5
SrGnis <srgnis@srgnis.xyz>
2026-02-11 18:49:01 +0100

    fix(ProjectVersionForm): update dependency type handling to use enum value

--- src/app/Livewire/ProjectVersionForm.php
+++ src/app/Livewire/ProjectVersionForm.php
@@ -145,7 +145,7 @@ class ProjectVersionForm extends Component
         foreach ($dependencies as $dependency) {
             if ($dependency->dependency_project_id || $dependency->dependency_project_version_id) {
                 $dependencyData = [
-                    'type' => $dependency->dependency_type,
+                    'type' => $dependency->dependency_type->value,
                     'mode' => 'linked',
                     'project_slug' => '',
                     'project_id' => null,
@@ -173,7 +173,7 @@ class ProjectVersionForm extends Component
                 }
             } else {
                 $dependencyData = [
-                    'type' => $dependency->dependency_type,
+                    'type' => $dependency->dependency_type->value,
                     'mode' => 'manual',
                     'project_slug' => '',
                     'project_id' => null,
