ac47dc6a687025cc1f04e5a9a9b5151be1fa87f3
SrGnis <srgnis@srgnis.com>
2026-05-26 22:39:58 +0200

    feat(ui): make recent versions clickable and improve download logic
    Wrap version names in links to allow direct navigation to the version
    details page. Updated the download button to target the first file
    associated with a version and added a tooltip for better UX.

--- src/resources/views/components/project-recent-versions.blade.php
+++ src/resources/views/components/project-recent-versions.blade.php
@@ -4,10 +4,18 @@
     @if($project->recent_versions->count() > 0)
         <div class="space-y-3">
             @foreach ($project->recent_versions as $version)
+                @php
+                    $firstFile = $version->files->first();
+                @endphp
                 <div class="flex justify-between items-start gap-3 pb-3 border-b border-base-content/10 last:border-b-0 last:pb-0">
                     <div class="flex-grow">
                         <div class="font-semibold text-sm">
-                            {{ $version->name }} - {{ $version->version }}
+                            <a
+                                href="{{ route('project.version.show', ['projectType' => $project->projectType, 'project' => $project, 'version_key' => $version]) }}"
+                                class="link link-hover"
+                            >
+                                {{ $version->name }} - {{ $version->version }}
+                            </a>
                         </div>
                         <div class="text-xs text-base-content/60 mt-1">
                             Released: {{ $version->release_date->format('Y-m-d') }}
@@ -16,12 +24,15 @@
                             {{ number_format($version->downloads) }} downloads
                         </div>
                     </div>
-                    <x-button
-                        link="{{ route('project.version.show', ['projectType' => $project->projectType, 'project' => $project, 'version_key' => $version]) }}"
-                        icon="download"
-                        class="btn-sm btn-primary"
-                        no-wire-navigate
-                    />
+                    @if ($firstFile)
+                        <x-button
+                            link="{{ route('file.download', ['projectType' => $project->projectType, 'project' => $project, 'version' => $version, 'file' => $firstFile]) }}"
+                            icon="download"
+                            class="btn-sm btn-primary"
+                            tooltip="Download {{ $firstFile->name }}"
+                            no-wire-navigate
+                        />
+                    @endif
                 </div>
             @endforeach
         </div>
@@ -32,4 +43,3 @@
         </div>
     @endif
 </x-card>
-
